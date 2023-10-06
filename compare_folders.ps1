ba9f77484f6669033d826ec2639df4b870510ac9
machado-nuno <138799833+machado-nuno@users.noreply.github.com>
2023-10-06 16:09:43 +0100

    Update compare_folders.ps1
    complete for testing

--- compare_folders.ps1
+++ compare_folders.ps1
@@ -12,6 +12,10 @@ Write-Host ("Total source files = ", $files.Count)
 
 
 $i=1
+$missing=0
+$notmatch=0
+$match= 0
+$total_files= $files.Count
 
 Write-Host("Count",$str_separator, "Status`t",$str_separator, "Source Hash`t", $str_separator, "Destination Hash`t", $str_separator, "Source`t", $str_separator, "Destination`t")
 
@@ -28,26 +32,69 @@ foreach ($file in $files) {
 
         if ($s_hash.Hash -eq $d_hash.Hash) {
            
-            $state= "OK"
+            $state= "Match"
             $mcolor = 'Green'
+            $match++
         }
         else {
             
-            $state= "NOK"
+            $state= "MisMatch"
             $mcolor = 'DarkRed'
+            $notmatch++
         }
 
-        Write-Host($i,"of",$files.Count, $str_separator, $state, $str_separator, $s_hash.Hash, $str_separator, $d_hash.Hash, $str_separator, $file.FullName, $str_separator, $d_file) -ForegroundColor $mcolor
+        Write-Host($i,"of",$total_files, $str_separator, $state, $str_separator, $s_hash.Hash, $str_separator, $d_hash.Hash, $str_separator, $file.FullName, $str_separator, $d_file) -ForegroundColor $mcolor
 
     }
     else {
 
-        Write-Host($i,"of",$files.Count, $str_separator, "Missing", $str_separator, $s_hash.Hash, $str_separator, "missing", $str_separator, $file.FullName, $str_separator, "missing") -ForegroundColor DarkRed
-
+        Write-Host($i,"of",$total_files, $str_separator, "Missing", $str_separator, $s_hash.Hash, $str_separator, "missing", $str_separator, $file.FullName, $str_separator, "missing") -ForegroundColor DarkRed
+        $missing++
     }
 
     $i++
 }
 
+Write-Host ("Summary stats")
+Write-Host ("Identical Files=", $match)
+Write-Host ("Diferent Files =", $notmatch)
+Write-Host ("Missing Files  =", $missing)
+Write-Host ("Total          =", $total_files)
+
+
 
 return $i
+
+
+$i=1
+$counter_ok = 0
+$counter_nok = 0
+
+foreach ($file in $files) {
+
+    $hash = Get-FileHash $file.FullName
+
+    $dest_file = $destination+ '\'+ $file.Name
+    
+    if (Test-Path $dest_file) {
+        Write-Host "File exists in destination"
+
+        $dest_hash = Get-FileHash $dest_file
+
+        if ($hash.hash -eq $dest_hash.hash) {
+            Write-Host "OK" $dest_file
+            $counter_ok++
+
+        } 
+        else {
+            Write-Host "NOK" $hash.hash $dest_hash.Hash
+            $counter_nok++
+        }
+    } 
+    else {
+        
+        Write-Host ('File ' +$file.Name+ " does not exist in destination")
+        $counter_nok++
+
+    }
+}
